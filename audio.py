10adf73cb166b5275f14e0dcc87dfd0979d5ecf7
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-17 13:38:14 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -166,8 +166,30 @@ async def ping(ctx):
     print ("user has pinged")
 
 @bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0x00ff00)
+    embed.set_author(name="Will Ryan of DAGames")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)    
+    
+@bot.command(pass_context=True)
 async def embed(ctx):
-    embed = discord.Embed(title="test", description="my name jeff", color=0x00ff00)
+    embed = discord.Embed(title="test", description="my name imran", color=0x00ff00)
     embed.set_footer(text="this is a footer")
     embed.set_author(name="Will Ryan of DAGames")
     embed.add_field(name="This is a field", value="no it isn't", inline=True)
