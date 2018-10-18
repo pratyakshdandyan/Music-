93181e82bcee5ef3f6f60ff6b3207a420e7ce3f0
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-18 12:07:59 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -193,7 +193,15 @@ async def kick(ctx, user: discord.Member):
     await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
     await bot.kick(user)
  
-
+@bot.command(pass_context=True)
+async def mute(ctx, user_id, userName: discord.User):
+    if ctx.message.author.server_permissions.administrator:
+        user = ctx.message.author
+        role = discord.utils.get(user.server.roles, name="Muted")
+        await client.add_roles(user, role)
+     else:
+       embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+       await bot.say(embed=embed)
 
 
 
