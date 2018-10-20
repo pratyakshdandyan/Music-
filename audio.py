a3cc054d319785ff1d02a4cc6a362207a9bf0a4d
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-20 12:13:58 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,13 +3,13 @@ import asyncio
 import youtube_dl
 import os
 import typing
-import json
+
 from discord.ext import commands
 from discord.ext.commands import Bot
 
 
 bot=commands.Bot(command_prefix='.')
-os.chdir(r'https://github.com/Iamanerror/Music-')
+
 
 from discord import opus
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
@@ -209,50 +209,7 @@ async def avatar(ctx, member: discord.Member):
     """User Avatar"""
     await bot.reply("{}".format(member.avatar_url))
 
-@bot.event
-async def on_member_join(member):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        await update_data(users, member)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-
-@bot.event
-async def on_message(message):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        if message.author.bot:
-            return
-        else:
-            await update_data(users, message.author)
-            number = random.randint(5,10)
-            await add_experience(users, message.author, number)
-            await level_up(users, message.author, message.channel)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-    await bot.process_commands(message)
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]["experience"] = 0
-        users[user.id]["level"] = 1
-
-async def add_experience(users, user, exp):
-    users[user.id]["experience"] += exp
-
-async def level_up(users, user, channel):
-    experience = users[user.id]["experience"]
-    lvl_start = users[user.id]["level"]
-    lvl_end = int(experience ** (1/4))
-
-    if lvl_start < lvl_end:
-        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
-        users[user.id]["level"] = lvl_end
+            
     
     
 @bot.command(pass_context=True)
