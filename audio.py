95dadd504b7e0b85f2d22eb622c3841f64532a71
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-20 23:58:31 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -231,22 +231,9 @@ async def coinflip(ctx, guess: str, amount: float):
         remove_dollars(author, amount)
 
 @bot.command(pass_contaxt=True)
-async def ban(self, ctx, user: discord.Member, *, reason:str=None):
-        """Bans the specified user from the server"""
-        if reason is None:
-            reason = Language.get("moderation.no_reason", ctx)
-        reason += Language.get("moderation.banned_by", ctx).format(ctx.author)
-        try:
-            await ctx.guild.ban(user, delete_message_days=0, reason=reason)
-        except discord.errors.Forbidden:
-            if user.top_role.position == ctx.me.top_role.position:
-                await ctx.send(Language.get("moderation.no_ban_highest_role", ctx))
-            elif user.top_role.position > ctx.me.top_role.position:
-                await ctx.send(Language.get("moderation.no_ban_higher_role", ctx))
-            else:
-                await ctx.send(Language.get("moderation.no_ban_perms", ctx))
-            return
-        await ctx.send(Language.get("moderation.ban_success", ctx).format(user))
+async def ban(self, ctx, user: discord.Member):
+    await bot.say("⚒️ booo!!, {}. Ya banned!".format(user.name))
+    await bot.ban(user)
   
     
     
