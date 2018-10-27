73231463b459e58465ffa10968f5970adea24c51
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-27 12:27:02 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -234,7 +234,16 @@ async def clear(ctx, number):
         mgs.append(x)
     await bot.delete_messages(mgs)
 
-
+@bot.command(pass_context = True)
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '455500545587675156':
+        role = discord.utils.get(member.server.roles, name='Muted')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
 async def embed(ctx):
