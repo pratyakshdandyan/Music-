b3e6ae524813c8b247e71bd32bc25e18fd0ba0bb
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2019-01-08 14:09:49 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -311,9 +311,58 @@ async def send_cmd_help(ctx):
                                color=discord.Color.blue())
             await bot.send_message(ctx.message.channel, embed=em)    
     
+@bot.command()
+async def guildcount():
+  	"""Bot Guild Count"""
+  	await bot.say("**I'm in {} Guilds!**".format(len(bot.servers)))  
     
     
     
+   
+ @bot.command(pass_context=True)
+async def guildid(ctx):
+	  """Guild ID"""
+	  await bot.say("`{}`".format(ctx.message.server.id))   
+    
+@bot.command(pass_context=True, no_pm=True)
+async def guildicon(ctx):
+    """Guild Icon"""
+    await bot.reply("{}".format(ctx.message.server.icon_url))
+    
+@bot.command(pass_context=True, hidden=True)
+async def setgame(ctx, *, game):
+    if ctx.message.author.id not in owner:
+        return
+    game = game.strip()
+    if game != "":
+        try:
+            await bot.change_presence(game=discord.Game(name=game))
+        except:
+            await bot.say("Failed to change game")
+        else:
+            await bot.say("Successfuly changed game to {}".format(game))
+    else:
+        await bot.send_cmd_help(ctx)    
+    
+    
+ @bot.command(pass_context=True, hidden=True)
+async def setname(ctx, *, name):
+    if ctx.message.author.id not in owner:
+        return
+    name = name.strip()
+    if name != "":
+        try:
+            await bot.edit_profile(username=name)
+        except:
+            await bot.say("Failed to change name")
+        else:
+            await bot.say("Successfuly changed name to {}".format(name))
+    else:
+        await bot.send_cmd_help(ctx)
+        
+        
+        
+        
     
   
     
