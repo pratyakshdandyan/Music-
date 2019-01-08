50c3d0e35b0a241775d0aaeaca5cdc56602a813c
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2019-01-08 14:55:57 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -361,7 +361,29 @@ async def setname(ctx, *, name):
         await bot.send_cmd_help(ctx)
         
         
-        
+
+
+newUserMessage = """ # customise this to the message you want to send new users
+You
+can
+put
+your
+multiline
+message
+here!
+"""
+
+@bot.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    await bot.send_message(member, newUserMessage)
+    print("Sent message to " + member.name)
+
+    # give member the steam role here
+    ## to do this the bot must have 'Manage Roles' permission on server, and role to add must be lower than bot's top role
+    role = discord.utils.get(member.server.roles, name="name-of-your-role")
+    await bot.add_roles(member, role)
+    print("Added role '" + role.name + "' to " + member.name)        
         
     
   
