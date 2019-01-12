b52039b3658ae08bb09b54fa3962e32ef8b6fdf4
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2019-01-12 14:37:37 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -64,7 +64,7 @@ async def checking_voice(ctx):
 @bot.event
 async def on_ready():
    bot.loop.create_task(all_false())
-   await bot.change_presence(game=discord.Game(name='Test'))
+   await bot.change_presence(game=discord.Game(name='Test2'))
    print(bot.user.name)
     
 @bot.command(pass_context=True)
@@ -92,38 +92,39 @@ async def player_in(con):  # After function for music
 
 
 @bot.command(pass_context=True)
-async def play(con, *, url):
-    """PLAY THE GIVEN SONG AND QUEUE IT IF THERE IS CURRENTLY SOGN PLAYING"""
-    if con.message.channel.is_private == True:
-        await bot.send_message(con.message.channel, "**You must be in a `server text channel` to use this command**")
+async def play(ctx, *,url):
 
-    if con.message.channel.is_private == False: #command is used in a server
-        rq_channel[con.message.server.id]=con.message.channel.id
-        if bot.is_voice_connected(con.message.server) == False:
-            await bot.join_voice_channel(con.message.author.voice.voice_channel)
+    opts = {
+        'default_search': 'auto',
+        'quiet': True,
+    }  # youtube_dl options
 
-        if bot.is_voice_connected(con.message.server) == True:
-            if player_status[con.message.server.id] == True:
-                song_names[con.message.server.id].append(url)
-                r = rq.Session().get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={}&key=put your youtube token here'.format(url)).json()
-                await bot.send_message(con.message.channel, "**Song `{}` Queued**".format(r['items'][0]['snippet']['title']))
 
-            if player_status[con.message.server.id] == False:
-                player_status[con.message.server.id] = True
-                song_names[con.message.server.id].append(url)
-                song = await bot.voice_client_in(con.message.server).create_ytdl_player(song_names[con.message.server.id][0], ytdl_options=opts, after=lambda: bot.loop.create_task(after_song(con, False, False)))
-                servers_songs[con.message.server.id] = song
-                servers_songs[con.message.server.id].start()
-                r = rq.Session().get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={}&key=AIzaSyDy4gizNmXYWykfUACzU_RsaHtKVvuZb9k'.format(url)).json()
-                pack = discord.Embed(title=r['items'][0]['snippet']['title'],
-                                     url="https://www.youtube.com/watch?v={}".format(r['items'][0]['id']['videoId']))
-                pack.set_thumbnail(
-                    url=r['items'][0]['snippet']['thumbnails']['default']['url'])
-                pack.add_field(name="Requested by:",
-                               value=con.message.author.name)
-                msg = await bot.send_message(con.message.channel, embed=pack)
-                now_playing[con.message.server.id] = msg
-                song_names[con.message.server.id].pop(0)
+    if ctx.message.server.id not in in_voice: #auto join voice if not joined
+        channel = ctx.message.author.voice.voice_channel
+        await bot.join_voice_channel(channel)
+        in_voice.append(ctx.message.server.id)
+
+    
+
+    if playing[ctx.message.server.id] == True: #IF THERE IS CURRENT AUDIO PLAYING QUEUE IT
+        voice = bot.voice_client_in(ctx.message.server)
+        song = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        songs[ctx.message.server.id]=[] #make a list 
+        songs[ctx.message.server.id].append(song) #add song to queue
+        await bot.say("```Audio {} is queued```".format(song.title))
+
+    if playing[ctx.message.server.id] == False:
+        voice = bot.voice_client_in(ctx.message.server)
+        player = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        players[ctx.message.server.id] = player
+        # play_in.append(player)
+        if players[ctx.message.server.id].is_live == True:
+            await bot.say("Can not play live audio yet.")
+        elif players[ctx.message.server.id].is_live == False:
+            player.start()
+            await bot.say("```Now playing audio```")
+            playing[ctx.message.server.id] = True
 
 
 
